593a379e627c9644c87227eb6c19ceaa405f152d
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2022-06-30 18:46:44 -0300

    first commit

---
+++ TreeCensus/Censo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeCensus
+{
+    public class Censo
+    {
+        public string Calle { get; set; }
+        public string Orientacion { get; set; }
+        public string Esquina1 { get; set; }
+        public string Esquina2 { get; set; }
+        public double AlturaCalle { get; set; }
+        public bool EsPar { get; set; }
+        public int Numero { get; set; }
+        public bool TieneVereda { get; set; }
+        public double MtsVereda { get; set; }
+        public bool HayZanja { get; set; }
+        public bool HayCordonCuneta { get; set; }
+        public bool HayLineaMediaTension { get; set; }
+        public bool HayPreensamblado { get; set; }
+        public bool HayInterConAlumbado { get; set; }
+        public string Especie { get; set; }
+        public string RUD { get; set; }
+        public string DLME { get; set; }
+        public string MaA { get; set; }
+        public string AaA { get; set; }
+        public bool LugarDisponible { get; set; }
+        public bool Cazuela { get; set; }
+        public bool Jardin { get; set; }
+        public double CircunfTronco { get; set; }
+        public AlturaEnum Altura { get; set; }
+        public PorcentajeEnum RamasDañadas { get; set; }
+        public PorcentajeEnum TroncoDañado { get; set; }
+        public string ElementosExtraños { get; set; }
+        public int InclinacionPeligrosa { get; set; }
+        public DañoPorRaicesEnum DañoPorRaices { get; set; }
+        public ProblemasFitosanitariosEnum ProblemasFitosanitarios { get; set; }
+        public string Otros { get; set; }
+        public EstadoGeneralEnum EstadoGeneral { get; set; }
+        public bool NecesitaTrabajoEnCopa { get; set; }
+        public bool NecesitaTrabajoEnBase { get; set; }
+    }
+}
+
+public enum AlturaEnum
+{
+    A6 = 6,
+    A12 = 12,
+    A18 = 18,
+    Mas18
+}
+
+public enum PorcentajeEnum
+{
+    P0 = 0,
+    P50 = 50,
+    p80 = 80
+}
+public enum DañoPorRaicesEnum
+{
+    Alto,
+    Medio,
+    Bajo,
+    Nada
+}
+public enum ProblemasFitosanitariosEnum
+{
+    Hongos,
+    Epifitas,
+    Insectos,
+    Nada
+}
+
+public enum EstadoGeneralEnum
+{
+    Bueno,
+    Regular,
+    Malo,
+    Muerto
+}
---
+++ TreeCensus/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TreeCensus
+{
+    public partial class MainPage : ContentPage
+    {
+        Censo censo = null;
+        bool _LugarDisponible = true;
+        public bool LugarDisponible
+        {
+            get { return _LugarDisponible; }
+            set { _LugarDisponible = value; OnPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            censo = new Censo();
+        }
+
+        private void ParImpar_Toggled(object sender, ToggledEventArgs e)
+        {
+            censo.EsPar = e.Value;
+        }
+
+        private void VeredaConstruida_Toggled(object sender, ToggledEventArgs e)
+        {
+            censo.TieneVereda = e.Value;
+        }
+
+        private void LugarDisponible_Toggled(object sender, ToggledEventArgs e)
+        {
+            censo.LugarDisponible = e.Value;
+            LugarDisponible = e.Value;
+            if (!e.Value)
+            {
+                censo.Cazuela = false;
+                censo.Jardin = false;
+            }
+        }
+    }
+}
